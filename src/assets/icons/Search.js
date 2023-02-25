3ce7a233db28f00b9470f26bb3a54206bed458b4
escebal <escebaldr@gmail.com>
2023-02-25 16:03:53 -0500

    add assets

---
+++ src/assets/icons/Search.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const SvgComponent = (props) => (
+  <svg width={48} height={48} xmlns="http://www.w3.org/2000/svg" {...props}>
+   
+    <path d="M0 0h48v1H0z" fill="#063855" fillRule="evenodd" />
+  </svg>
+)
+
+export default SvgComponent
